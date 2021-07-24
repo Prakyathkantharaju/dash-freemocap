5c58db93a00c83b64ab78f04a6bada1f664d93b2
Prakyath <prakyath@pop-os.localdomain>
2021-07-23 22:09:31 -0500

    added jumping jack

--- plot.py
+++ plot.py
@@ -4,16 +4,17 @@ import pandas as pd
 
 from import_c3d import MarkerSet
 
-marker = MarkerSet('hips.c3d')
+marker = MarkerSet('jumping_jacks.c3d')
 print(marker.markerLabels)
-data = marker.getMarkerList(marker.markerLabels[:],20)
 
 # 3d projection
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-for i, points in enumerate(data):
-    print(i)
-    ax.scatter(points[0], points[1], points[2])
-
-plt.show()
+for i in range(2,marker.endFrame, 10):
+    print(marker.frameRate)
+    data = marker.getMarkerList(marker.markerLabels[:],i)
+    for j, points in enumerate(data):
+        ax.scatter(points[0], points[1], points[2])
+    plt.pause(0.000001)
+    ax.cla()
